241c35e1e9c4c523ed2e2b1adca18f951464b97e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-04-05 02:30:28 +0100

    Enable vibrator function

---
+++ vibrator.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2011 CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define THE_DEVICE "/sys/devices/platform/star_vib_name/stay"
+
+int sendit(int timeout_ms)
+{
+    int nwr, ret, fd;
+    char value[20];
+
+    
+    fd = open(THE_DEVICE, O_RDWR);
+    if(fd < 0)
+        return errno;
+
+    nwr = sprintf(value, "%d\n", timeout_ms);
+    ret = write(fd, value, nwr);
+
+    close(fd);
+
+    return (ret == nwr) ? 0 : -1;
+}
