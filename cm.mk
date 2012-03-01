bfb1934bdd55c1352550d06d8367c4cb2a00826b
Ilia Aksyonov <axel2033@gmail.com>
2012-03-01 14:24:38 +0400

    update device info

--- cm.mk
+++ cm.mk
@@ -7,13 +7,14 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/lge/p990/p990.mk)
 
-PRODUCT_NAME := cm_p990
-
-# Release name and versioning
+# Release name
 PRODUCT_RELEASE_NAME := Optimus2X
-PRODUCT_VERSION_DEVICE_SPECIFIC :=
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p990
+PRODUCT_NAME := cm_p990
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := Optimus 2X
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_star BUILD_ID=GRJ22 BUILD_FINGERPRINT=lge/lge_star/p990_EUR-xx:2.3.4/GRJ22/lgp990-V20l.422C563E:user/release-keys PRIVATE_BUILD_DESC="lge_star-user 2.3.4 GRJ22 422C563E release-keys"
