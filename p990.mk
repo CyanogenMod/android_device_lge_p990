c6c5855a6d5b1dd3cca53401d25c614097c39aac
Tom Giordano <tomgiordano83@gmail.com>
2012-04-28 10:28:30 +1000

    p990: build kernel and add cm.dependencies
    Change-Id: I3f731b786d5a05438c5ae86c9f3263be66f722de

--- p990.mk
+++ p990.mk
@@ -8,12 +8,6 @@ DEVICE_PACKAGE_OVERLAYS += device/lge/p990/overlay
 # Inherit non-open-source blobs.
 $(call inherit-product-if-exists, vendor/lge/p990/p990-vendor.mk)
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/lge/p990/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 # Board-specific init
 PRODUCT_COPY_FILES += \
     device/lge/p990/init.p990.rc:root/init.p990.rc \
@@ -23,10 +17,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/init.vsnet:system/bin/init.vsnet \
     $(LOCAL_PATH)/gps_brcm_conf.xml:system/etc/gps_brcm_conf.xml
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel \
-    $(LOCAL_PATH)/prebuilt/wireless.ko:system/lib/modules/wireless.ko
 
 PRODUCT_PACKAGES += \
     lights.p990 \
