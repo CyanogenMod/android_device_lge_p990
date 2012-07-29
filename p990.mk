f4190316ef07573a50ffb235166430dc3bb26f7a
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-07-29 15:43:49 +0100

    p990: Updates for jellybean

--- p990.mk
+++ p990.mk
@@ -22,6 +22,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/init.vsnet:system/bin/init.vsnet \
+    $(LOCAL_PATH)/init.vsnet-down:system/bin/init.vsnet-down \
     $(LOCAL_PATH)/gps_brcm_conf.xml:system/etc/gps_brcm_conf.xml
 
 PRODUCT_COPY_FILES += \
